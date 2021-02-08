d7b8de0e973ebc3a27d278665c66a1330d3ea5b8
Niyamaka <niyamaka@yuneta.io>
2021-02-08 18:14:51 +0100

    v4.8.8 Permissions ENABLED WARNING

--- src/main.c
+++ src/main.c
@@ -19,7 +19,7 @@
 "Si quieres vivir en mi reino tienes que cumplir unas reglas."\
 "De lo contrario, vive como un standalone." \
 
-#define APP_VERSION     "4.8.7"
+#define APP_VERSION     "4.8.8"
 #define APP_DATETIME    __DATE__ " " __TIME__
 #define APP_SUPPORT     "<niyamaka at yuneta.io>"
 

71d843f08042c676e81d7ba81b515bf0e4588439
Niyamaka <niyamaka@yuneta.io>
2021-07-29 12:20:56 +0200

    Protect sys.machine and change _ by -

--- src/main.c
+++ src/main.c
@@ -289,7 +289,7 @@ PRIVATE char variable_config[]= "\
                                                     'trace': false              \n\
                                                 },                              \n\
                                                 'urls':[                        \n\
-        'tcps://(^^__node_owner__^^)(^^__sys_machine__^^).(^^__output_url__^^)'    \n\
+        'tcps://(^^__sys_machine__^^).(^^__node_owner__^^).(^^__output_url__^^)'    \n\
                                                 ]                               \n\
                                             }                                   \n\
                                         }                                       \n\

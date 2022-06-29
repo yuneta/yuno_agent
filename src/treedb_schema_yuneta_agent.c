1b12e733a210b56d10c3edc7cdf9f9afc7824c14
ginsmar <ginsmar@gmail.com>
2022-06-29 09:25:54 +0200

    incr pid field to 6 chars

--- src/treedb_schema_yuneta_agent.c
+++ src/treedb_schema_yuneta_agent.c
@@ -147,7 +147,7 @@
 static char treedb_schema_yuneta_agent[]= "\
 {                                                                   \n\
     'id': 'treedb_yuneta_agent',                                    \n\
-    'schema_version': '11',                                         \n\
+    'schema_version': '12',                                         \n\
     'topics': [                                                     \n\
         {                                                           \n\
             'id': 'realms',                                         \n\
@@ -275,7 +275,7 @@ static char treedb_schema_yuneta_agent[]= "\
             'id': 'yunos',                                          \n\
             'pkey': 'id',                                           \n\
             'system_flag': 'sf_string_key',                         \n\
-            'topic_version': '8',                                   \n\
+            'topic_version': '9',                                   \n\
             'tkey': '',                                             \n\
             'pkey2s': 'yuno_release',                               \n\
             'cols': {                                               \n\
